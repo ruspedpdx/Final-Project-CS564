194bd1544ba324b48ad2e4b96fa8b584afe1d5ea
datdinh7719 <datdinh@pdx.edu>
2024-12-08 10:31:46 -0800

    Final Submission with all features

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,26 @@
-/* eslint-disable react/jsx-filename-extension */
 import React from "react";
+import { Button, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className=" text-center">
-      <h2> Final Project</h2>
-      <h4>draft for the final project.</h4>
-    </div>
+    <Container className="text-center my-5 py-5">
+      <h2>Welcome to Education Expenses</h2>
+      <h4>Your go-to place to search for information about colleges.</h4>
+
+      <div className="mt-4">
+        <Link to="/search">
+          <Button variant="primary" size="lg" className="mx-2">
+            Search by School
+          </Button>
+        </Link>
+        <Link to="/raghad">
+          <Button variant="secondary" size="lg" className="mx-2">
+            View Schools by State
+          </Button>
+        </Link>
+      </div>
+    </Container>
   );
 }
 
